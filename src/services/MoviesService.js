65f9599da0662ef615a84a00a602e0e76e1c68e6
Sabolc Keringer <keringersz@gmail.com>
2019-12-10 20:44:14 +0100

    Zadatak 2: Services created

---
+++ src/services/MoviesService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000/api'
+
+class MoviesServices {
+    constructor() {
+        this.configureAxios(
+            BASE_URL,
+            {
+                accept: 'application/json',
+                authorization: `Bearer ${ localStorage.getItem('token') }`
+            }
+        )
+    }
+
+    configureAxios(baseURL, headers = {}) {
+        axios.defaults.baseURL = baseURL
+        Object.assign {
+            axios.defaults.headers.common,
+            headers
+        }
+    }
+}
+
+export const moviesService = new MoviesServices()
